b5beb0f09ec283ceaacd8a1d0c5e6970cc575cdd
wjb321 <1453521207@qq.com>
2022-03-14 04:11:32 +0800

    2 nodes, one node generates wrong data, and the ID has problem

---
+++ CAN/LoopBack/intermediate/wrong_data+ID/USER/stm32f10x_it.c
@@ -0,0 +1,219 @@
+/**
+  ******************************************************************************
+  * @file    Project/STM32F10x_StdPeriph_Template/stm32f10x_it.c 
+  * @author  MCD Application Team
+  * @version V3.4.0
+  * @date    10/15/2010
+  * @brief   Main Interrupt Service Routines.
+  *          This file provides template for all exceptions handler and 
+  *          peripherals interrupt service routine.
+  ******************************************************************************
+  * @copy
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2010 STMicroelectronics</center></h2>
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f10x_it.h"
+
+/** @addtogroup STM32F10x_StdPeriph_Template
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+
+/******************************************************************************/
+/*            Cortex-M3 Processor Exceptions Handlers                         */
+/******************************************************************************/
+
+/**
+  * @brief   This function handles NMI exception.
+  * @param  None
+  * @retval None
+  */
+void NMI_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Hard Fault exception.
+  * @param  None
+  * @retval None
+  */
+void HardFault_Handler(void)
+{
+  /* Go to infinite loop when Hard Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Memory Manage exception.
+  * @param  None
+  * @retval None
+  */
+void MemManage_Handler(void)
+{
+  /* Go to infinite loop when Memory Manage exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Bus Fault exception.
+  * @param  None
+  * @retval None
+  */
+void BusFault_Handler(void)
+{
+  /* Go to infinite loop when Bus Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles Usage Fault exception.
+  * @param  None
+  * @retval None
+  */
+void UsageFault_Handler(void)
+{
+  /* Go to infinite loop when Usage Fault exception occurs */
+  while (1)
+  {
+  }
+}
+
+/**
+  * @brief  This function handles SVCall exception.
+  * @param  None
+  * @retval None
+  */
+void SVC_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles Debug Monitor exception.
+  * @param  None
+  * @retval None
+  */
+void DebugMon_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles PendSVC exception.
+  * @param  None
+  * @retval None
+  */
+void PendSV_Handler(void)
+{
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+}
+
+/******************************************************************************/
+/*                 STM32F10x Peripherals Interrupt Handlers                   */
+/*  Add here the Interrupt Handler for the used peripheral(s) (PPP), for the  */
+/*  available peripheral interrupt handler's name please refer to the startup */
+/*  file (startup_stm32f10x_xx.s).                                            */
+/******************************************************************************/
+
+/**
+  * @brief  This function handles PPP interrupt request.
+  * @param  None
+  * @retval None
+  */
+/*void PPP_IRQHandler(void)
+{
+}*/
+
+/*******************************************************************************
+* Function Name  : USB_LP_CAN1_RX0_IRQHandler
+* Description    : This function handles USB Low Priority or CAN RX0 interrupts 
+* Input          : None
+* Output         : None
+* Return         : None
+* Attention		 : None
+*******************************************************************************/
+//extern uint8_t CanFlag, CAN_ID;
+//extern uint8_t CAN_DATA0,CAN_DATA1,CAN_DATA2,CAN_DATA3,CAN_DATA4,CAN_DATA5,CAN_DATA6,CAN_DATA7;
+//void USB_LP_CAN1_RX0_IRQHandler(void) 
+//{
+//  CanRxMsg RxMessage;
+//  CAN_Receive(CAN1,CAN_FIFO0, &RxMessage);  /* 此函数包含释放提出报文了的,在非必要时,不需要自己释放 */
+//  CAN_ID=RxMessage.StdId;
+//	
+//	if(RxMessage.StdId==0x14  )//&& (RxMessage.IDE==CAN_ID_STD) && (RxMessage.DLC==8)
+//	{
+//	CanFlag = ENABLE;					       //接收成功  
+//	}
+//	else
+//	{
+//	CanFlag = DISABLE;						   //接收失败
+//	}
+//	//CAN_ID=RxMessage.ExtId;
+//  CAN_DATA0=RxMessage.Data[0];
+//  CAN_DATA1=RxMessage.Data[1];
+//  CAN_DATA2=RxMessage.Data[2];
+//  CAN_DATA3=RxMessage.Data[3];
+//  CAN_DATA4=RxMessage.Data[4];
+//  CAN_DATA5=RxMessage.Data[5];
+//  CAN_DATA6=RxMessage.Data[6];
+//  CAN_DATA7=RxMessage.Data[7];
+//  CAN_ClearITPendingBit(CAN1,CAN_IT_FMP0);  /* 清除挂起中断 */
+//  CanFlag = ENABLE;
+//}
+
+extern uint8_t CanFlag, CAN_ID;
+extern uint8_t CAN_DATA0,CAN_DATA1,CAN_DATA2,CAN_DATA3,CAN_DATA4,CAN_DATA5,CAN_DATA6,CAN_DATA7;
+void USB_LP_CAN1_RX0_IRQHandler(void) 
+{
+  CanRxMsg RxMessage;
+  CAN_Receive(CAN1,CAN_FIFO0, &RxMessage);  /* 此函数包含释放提出报文了的,在非必要时,不需要自己释放 */
+  CAN_ID=RxMessage.StdId;
+  CAN_DATA0=RxMessage.Data[0];
+  CAN_DATA1=RxMessage.Data[1];
+  CAN_DATA2=RxMessage.Data[2];
+  CAN_DATA3=RxMessage.Data[3];
+  CAN_DATA4=RxMessage.Data[4];
+  CAN_DATA5=RxMessage.Data[5];
+  CAN_DATA6=RxMessage.Data[6];
+  CAN_DATA7=RxMessage.Data[7];
+  CAN_ClearITPendingBit(CAN1,CAN_IT_FMP0);  /* 清除挂起中断 */
+  CanFlag = ENABLE;
+}
+
+
+
+
+
+/**
+  * @}
+  */ 
+
+
+/******************* (C) COPYRIGHT 2010 STMicroelectronics *****END OF FILE****/
---
+++ CAN/LoopBack/intermediate/wrong_data+ID/readme.txt
@@ -0,0 +1,10 @@
+
+Problems:
+
+do the loopback test, there are two different nodes A, B.
+1) try these nodes with this code, but A generates the data is what I want, B has different data in CAN_DATA6 and 
+CAN_DATA7 parts, when there are CAN_DATA0-7
+
+2) 2 nodes they both have changed ID values. 
+    for example: I set ID = 0x22,  while the serial ports show ID is 0xXX22, where XX means random values from 0-e in hex.
+    
