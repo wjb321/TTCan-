d2920a68411c24c0f5579612e26601cc5d8f082a
wjb321 <1453521207@qq.com>
2022-07-07 06:15:55 +0800

    ttcan adds encoder mode, still did not used the speed info from node2

---
+++ TTCAN/TTCAN_V2.2/USER/timer/timer.h
@@ -0,0 +1,15 @@
+#ifndef __TIMER_H
+#define __TIMER_H
+#include "sys.h"
+
+
+ int read_encoder(void); //static
+int calc_motor_rotate_speed();
+
+
+void TIM3_Int_Init(u16 arr,u16 psc, u8 timer_enable_flag);
+void TIM2_Int_Init(u16 arr,u16 psc, u8 timer_enable_flag);  // node0 or node 2(high frequency)
+void TIM4_Int_Init(u16 arr,u16 psc); //basic config
+void TIM4_PWM_Init(u16 arr,u16 psc); //node1
+void TIM4_EncoderMode_Config(void);  //node2
+#endif
